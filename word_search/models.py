eb2a1b39a666572c408f03185c2ebd0f7dab60b9
Danlin Chen <d84chen@uwaterloo.com>
2019-06-11 13:03:51 +0800

    word puzzle wording

--- word_search/models.py
+++ word_search/models.py
@@ -34,7 +34,7 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    cows_found1 = models.IntegerField(min=0, max=100, label='How many cows did you find in the picture? You will get ' + str(Constants.prize) + ' for each cow you find.')
+    cows_found1 = models.IntegerField(min=0, max=100, label='How many cows did you find in the picture? You will get 100 ECUs times the number you select on the pull-down menu.')
     cows_found2 = models.IntegerField(
         label='How many cows did you find in the picture? You will get ' + str(Constants.prize) +  ' for each cow you find.',
         choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
